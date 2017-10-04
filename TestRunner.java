22ed954e87e2c731ffcb0c5d125587c0a6566c80
JD <jkd28@pitt.edu>
2017-10-04 10:22:29 -0400

    Add necessary line to TestRunner

--- TestRunner.java
+++ TestRunner.java
@@ -10,15 +10,16 @@ public class TestRunner {
 	boolean anyFailures = false;
 
 	// ADD ANY CLASSES YOU WISH TO TEST HERE
-		
+	classesToTest.add(RentACat.class);
+    
 	// For all test classes added, loop through and use JUnit
 	// to run them.
-	
+
 	for (Class c: classesToTest) {
 	    Result r = JUnitCore.runClasses(c);
 
 	    // Print out any failures for this class.
-      
+
 	    for (Failure f : r.getFailures()) {
 		System.out.println(f.toString());
 	    }
@@ -28,19 +29,19 @@ public class TestRunner {
 	    // can never be set back to false (no amount of
 	    // successes will ever eclipse the fact that there
 	    // was at least one failure.
-	    
+
 	    if (!r.wasSuccessful()) {
 		anyFailures = true;
 	    }
-	    
+
 	}
-	
+
 	// After completion, notify user if all tests passed or any failed.
-	
+
 	if (anyFailures) {
 	    System.out.println("\n!!! - At least one failure, see above.");
 	} else {
 	    System.out.println("\nALL TESTS PASSED");
-	} 
+	}
     }
-} 
+}
