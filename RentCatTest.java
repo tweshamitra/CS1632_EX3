6ecdde5ee7b3d705d25097aca487e95f09b1d86c
Twesha Mitra <twesha95@gmail.com>
2017-10-05 00:23:00 -0400

    finished up tests

--- RentCatTest.java
+++ RentCatTest.java
@@ -81,27 +81,37 @@ public class RentCatTest{
         boolean cat2Exist = _rentACat.catExists(1, catArray);
         assertEquals(true, cat1Exist && cat2Exist);
     }
-}
-    RentACat rc = new RentACat();
 
+    
+    //tests whether or not user can rent a cat that has not been rented
     @Test
-    public void testRentCat(){
-        Cat c = Mockito.mock(Cat.class);
-        boolean flag = rc.returnCat(c);
-        assertEquals(flag, true);
+    public void testRentRentedCat(){
+        Cat c = mock(Cat.class);
+        when(c.getRented()).thenReturn(true);
+        assertEquals(_rentACat.rentCat(c), false);
     }    
 
+    //tests whether or not user can rent a cat that has already been rented
     @Test 
-    public void testInvalidCat(){
-        Cat c = Mockito.mock(Cat.class);
-        boolean flag = rc.returnCat(c);
-        assertEquals(flag, false);
+    public void testRentUnrentedCat(){
+        Cat c = mock(Cat.class);
+        when(c.getRented()).thenReturn(false);
+        assertEquals(_rentACat.rentCat(c), true);;
     }
 
+    //tests whether or not user can return a cat that has been rented
     @Test 
-    public void testReturnInvalidCat(){
-        Cat c = Mockito.mock(Cat.class);
-        boolean flag = rc..returnCat(c);
-        assertEquals(flag, false);
+    public void testReturnRentedCat(){
+        Cat c = mock(Cat.class);
+        when(c.getRented()).thenReturn(true);
+        assertEquals(_rentACat.returnCat(c), true);
+    }
+
+    //tests whether or not user can return a cat that has not been rented
+    @Test
+    public void testReturnUnrentedCat(){
+        Cat c = mock(Cat.class);
+        when(c.getRented()).thenReturn(false);
+        assertEquals(_rentACat.returnCat(c), false);
     }
 }
